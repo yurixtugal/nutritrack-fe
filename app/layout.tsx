fbde599021249bbff770b7ac9932e5018998e583
yurixtugal <yurixtugal12@gmail.com>
2024-05-11 08:30:18 -0500

    auth routes were created and sign-in and sign-up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import { ClerkProvider } from '@clerk/nextjs'
+
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -15,8 +18,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
+    </ClerkProvider>
   );
 }
